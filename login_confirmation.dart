f9968d42c73561dfe25f20c020842e94d0fcaefa
syedahkhadijashah@gmail.com <syedahkhadijashah@gmail.com>
2024-08-17 20:28:34 +0500

    modify login_confirmation

--- login_confirmation.dart
+++ login_confirmation.dart
@@ -1,21 +1,18 @@
 void main() {
-  String email1 = "abcgmail.com";
-  String password1 = "maham";
+  String email1 = "abc@gmail.com";
+  String password1 = "mah#am";
   String email2 = "abc@gmail.com";
   String password2 = "maham";
-  bool result = email1 == email2;
-  bool result2 = password1 == password2;
- if (result && result2 == false) {
-      if (email1 != email2 && password1 != password2)
-        print("wronf email and password");
-      else if (email1 != email2) 
-        print("wrong email");
-      else
-        print("wrong password");}
-  else {
-     if (result == result2) {
+  // bool result = email1 == email2;
+  // bool result2 = password1 == password2;
+  if (email1 != email2 || password1 != password2) {
+    if (password1 != password2 && email1 != email2) {
+      print("wronf email and password");
+    } else if (email1 != email2) {
+      print("wrong email");
+    } else
+      print("wrong password");
+  } else {
     print("login successfully");
-  } 
-    }
   }
-
+}
