8f2bbf7d10ead1641341e068759beda2af95bc35
syedahkhadijashah@gmail.com <syedahkhadijashah@gmail.com>
2024-08-23 21:23:47 +0500

    update list_arry.dart

--- list_arry.dart
+++ list_arry.dart
@@ -9,7 +9,7 @@ main() {
 //   // studentnames.sort();
 //   // print(studentnames);
 
-  List studentnames = [56, 48, 75, 36, 242];
+  //List studentnames = ["safia", "maham", "khadija", "mehreen", "batool"];
   // for sorting
 //   var sum = List.of(studentnames);
 //  sum.sort();
@@ -22,7 +22,46 @@ main() {
   //   print(studentnames.isEmpty);
 
   //  for adding element
-  studentnames.add("hellow");
-  print(studentnames);
-  studentnames.addAll(["ddd", "abc", "safia"]);
+  // studentnames.add("hellow");
+  // print(studentnames);
+  // studentnames.addAll(["ddd", "abc", "safia"]);
+  //studentnames.insert(0, "abc");
+  // studentnames.insertAll(2, ["a", "b"]);
+  // print(studentnames);
+
+  // for reverse
+  //is tarhan list ko agar assign karky reverse karyn to hamayn list of karky karna hta hai
+  // var abc=List.of(studentnames.reversed);
+  // abc.add("kkk");
+  // print(abc);
+
+  //  for removing
+  // studentnames.remove("khadija");
+  // print(studentnames);
+  // studentnames.removeAt(1);
+  //   print(studentnames);
+
+//removerwhere with condition
+  // List studentnames = [5, 6, 9, 55, 2, 3];
+  // // studentnames.removeWhere((e) => e > 5);
+  // studentnames.retainWhere((e) => e < 5);
+  // print(studentnames);
+
+//sort at descending order
+ // List studentnames = [1, 5, 2, 4, 3, 9, 8, 6, 7, 0];
+  // studentnames.sort();
+  // //  print(studentnames);
+  // var abc=List.of(studentnames.reversed);
+  //   print(abc);
+  
+  //yeh list.of karky or .tolist karky dono sy large bracket mai convert hoti hai list ki type
+// List studentnames= [1, 2, 3, 4, 5, 6];
+// var evenNumbers = List.of(studentnames.where((n)=>n.isEven ));
+// print(evenNumbers);  // Output: [2, 4, 6]
+//or 
+// List<int> numbers = [1, 2, 3, 4, 5, 6];
+// var evenNumbers = numbers.where((number) => number.isEven);
+// print(evenNumbers.toList());  // Output: [2, 4, 6]
+
+
 }
