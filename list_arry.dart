b8af94d2352edbcbb0da40d798971b34a618f67e
syedahkhadijashah@gmail.com <syedahkhadijashah@gmail.com>
2024-08-23 14:25:49 +0500

    create list_arry.dart

---
+++ list_arry.dart
@@ -0,0 +1,28 @@
+main() {
+//   List studentnames = ["safia", "maham", "kainat", "kulsoom", "warisha"];
+//   // print(studentnames[studentnames.length - 2]);
+
+//   // studentnames
+//   //     .replaceRange(studentnames.length - 1, studentnames.length, ["def"]);
+//   // print(studentnames);
+
+//   // studentnames.sort();
+//   // print(studentnames);
+
+  List studentnames = [56, 48, 75, 36, 242];
+  // for sorting
+//   var sum = List.of(studentnames);
+//  sum.sort();
+//   print(sum);
+
+  // for empty and not empty
+  // print(studentnames.isEmpty);
+
+  // studentnames.Clear;
+  //   print(studentnames.isEmpty);
+
+  //  for adding element
+  studentnames.add("hellow");
+  print(studentnames);
+  studentnames.addAll(["ddd", "abc", "safia"]);
+}
